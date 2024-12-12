a00de32ed6c14958fe4ec5e8e38b5f33fec08fc5
Wajan <wajanaltalhan@gmail.com>
2024-12-12 10:58:55 +0300

    splash screen is done

---
+++ WakebApp/splashscreenview.swift
@@ -0,0 +1,18 @@
+//
+//  splashscreenview.swift
+//  WakebApp
+//
+//  Created by Wajan Altalhan on 11/06/1446 AH.
+//
+
+import SwiftUI
+
+struct splashscreenview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    splashscreenview()
+}
