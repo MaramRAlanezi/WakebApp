8ea4d498a59b6261feea8a3b773c6f93d25816de
Wajan <wajanaltalhan@gmail.com>
2024-12-16 10:56:13 +0300

    new version

--- WakebApp/WakebAppApp.swift
+++ WakebApp/WakebAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WakebAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            splashscreenview()
         }
     }
 }
--- WakebApp/splashscreenview.swift
+++ WakebApp/splashscreenview.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct splashscreenview: View {
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isActive {
+            ContentView()
+        } else {
+            ZStack{
+                Color.color
+                    .ignoresSafeArea()
+            VStack{
+                VStack{
+                    Image("logo2")
+                    Text("Grow With Every Word")
+                        .font(.system(size: 30))
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 2.5)) {
+                        self.size = 0.9
+                        self.opacity = 1.0
+                    }
+                }
+                }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
+                    withAnimation{
+                        self.isActive = true  }
+                }
+            }
+            }
+        }
+      
     }
 }
 
